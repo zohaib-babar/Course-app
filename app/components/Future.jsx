b84c4b31e389ac288296db15537b1cddc590cebe
zohaib-babar <zohaibbabar3388@gmail.com>
2025-12-25 21:44:56 +0500

    solve error

--- app/components/Future.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Future() {
-  return (
-    <div><h1>heading fromt he div</h1></div>
-  )
-}
-
-export default Future
--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,17 @@
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
-import Future from "./components/Future"
+import Features from "./components/Features"
+import Team from "./components/Team"
+
 export default function ResumePage() {
   return (
       <div>
         <Navbar />
         <Banner />
+        <Team />
+        <Features />
         <Footer />
-        <Future />
       </div>
   );
 }
